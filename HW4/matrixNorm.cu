775a6d3bfb3daeb53481c3b5fd75ab9ec9668c3e
Jason-Lawrence <thejasonlawrence1@gmail.com>
2019-11-26 17:16:54 -0600

    fixed seg fault

--- HW4/matrixNorm.cu
+++ HW4/matrixNorm.cu
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define BLOCK_SIZE 16
+#define GRID_SIZE 1
+
 int printMatrix(int *A, int N){
   int i, j;
   printf("\nA = \n\t");
   for(i = 0; i < N; ++i){
     for(j = 0; j < N; ++j){
-      printf("%5.2f%s", A[i * N + j], j < N-1 ? ", " : ";\n\t");
+      if(j < N-1) printf("%d, ", A[i * N + j]);
+      else printf("%d;\n\t", A[i * N + j]);
+      //printf("%d, ", A[i * N + j], j < N-1 ? ", " : ";\n\t");
     }
   }
   return 0;
@@ -16,6 +21,7 @@ int printMatrix(int *A, int N){
 __global__ void gpu_matrixNorm(int *A, int *B, int N){
   int row, col;
   float mu, sigma;
+  
   mu = 0.0;
   col = blockIdx.x * blockDim.x + threadIdx.x;
   for(row = 0; row < N; ++row){
@@ -36,23 +42,26 @@ __global__ void gpu_matrixNorm(int *A, int *B, int N){
 
 int main(int argc, char const *argv[]){
   printf("Begin\n");
-  int N = 8; //Matrix size
+  int N = 4; //Matrix size
   
   srand(300); // Fixed Seed
   // allocate memory in host RAM
-  int *h_a, *h_b;
-  cudaMallocHost((int **) &h_a, sizeof(int)*N*N);
-  cudaMallocHost((int **) &h_b, sizeof(int)*N*N);
-
+  int *A, *B;
+  A = (int*)malloc(N*N*sizeof(int));
+  B = (int*)malloc(N*N*sizeof(int));
+  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 dimGrid(GRID_SIZE, GRID_SIZE); 
   //generate matrix A
   printf("generating A\n");
   for(int i = 0; i < N; ++i){
     for(int j = 0; j < N; ++j){
-      h_a[i * N + j] = rand() / 32768;
+      A[i * N + j] = rand() / 32768;
     }
   }
+
   printf("A generated\n");
-  printMatrix(h_a, N);
+  printMatrix(A, N);
+  printMatrix(B, N); 
   float gpu_elapsed_time_ms;
 
   //events to count the execution time
@@ -62,29 +71,30 @@ int main(int argc, char const *argv[]){
   
   //allocate memory on the device
   int *d_a, *d_b;
-  cudaMalloc((int **) &d_a, sizeof(int)*N*N);
-  cudaMalloc((int **) &d_b, sizeof(int)*N*N);
+  cudaMalloc((void **)&d_a, sizeof(int)*N*N);
+  cudaMalloc((void **)&d_b, sizeof(int)*N*N);
 
 
-  cudaMemcpy(d_a, h_a, sizeof(int)*N*N, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_a, A, sizeof(int)*N*N, cudaMemcpyHostToDevice);
   
   cudaEventRecord(start, 0);
   // Call Matrix Norm function
-  int numBlocks = N / 4;
-  int numThreads = N / 4;
-  gpu_matrixNorm<<<numBlocks, numThreads>>>(d_a, d_b, N);
+  int numBlocks = N / 2;
+  int numThreads = N / 2;
+  gpu_matrixNorm<<<dimBlock, dimGrid>>>(d_a, d_b, N);
 
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   
-  cudaMemcpy(h_b, d_b, sizeof(int)*N*N, cudaMemcpyDeviceToHost);
+  cudaMemcpy(B, d_b, sizeof(int)*N*N, cudaMemcpyDeviceToHost);
 
   cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
-  printf("Runtime = %f ms.\n\n", gpu_elapsed_time_ms);
-
+  printMatrix(B, N);
+  printf("Runtime = %f ms.\n", gpu_elapsed_time_ms);
+  
   cudaFree(d_a);
   cudaFree(d_b);
-  cudaFreeHost(h_a);
-  cudaFreeHost(h_b);
+  free(A);
+  free(B);
   return 0;
 }
