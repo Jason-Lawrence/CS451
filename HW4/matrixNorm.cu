50ab16914d780d56aa831456e5894cce71d8fe9f
Jason-Lawrence <thejasonlawrence1@gmail.com>
2019-11-26 21:20:55 -0600

    done

--- HW4/matrixNorm.cu
+++ HW4/matrixNorm.cu
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <time.h>
 
 #define BLOCK_SIZE 16
 #define GRID_SIZE 1
 
 int printMatrix(int *A, int N){
+  if(N > 8) return 0;
   int i, j;
   printf("\nA = \n\t");
   for(i = 0; i < N; ++i){
@@ -19,82 +25,85 @@ int printMatrix(int *A, int N){
 }
 
 __global__ void gpu_matrixNorm(int *A, int *B, int N){
+  
   int row, col;
   float mu, sigma;
-  
   mu = 0.0;
   col = blockIdx.x * blockDim.x + threadIdx.x;
-  for(row = 0; row < N; ++row){
-    mu += A[row * N + col];
-  }
-  mu /= (float) N;
-  sigma = 0.0;
-  for(row = 0; row < N; ++row){
-    sigma += powf(A[row * N + col] - mu, 2.0);
-  }
-  sigma /= (float) N;
-  sigma = sqrt(sigma);
-  for(row = 0; row < N; ++row){
-    if(sigma == 0.0) B[row * N + col] = 0.0;
-    else B[row * N + col] = (A[row * N + col] - mu) / sigma;
+  if(col < N){
+    for(row = 0; row < N; ++row){
+      mu += A[row * N + col];
+    }
+    mu /= (float) N;
+    sigma = 0.0;
+    for(row = 0; row < N; ++row){
+      sigma += powf(A[row * N + col] - mu, 2.0);
+    }
+    sigma /= (float) N;
+    sigma = sqrt(sigma);
+    for(row = 0; row < N; ++row){
+      if(sigma == 0.0) B[row * N + col] = 0.0;
+      else B[row * N + col] = (A[row * N + col] - mu) / sigma;
+    }
   }
 }
 
-int main(int argc, char const *argv[]){
-  printf("Begin\n");
-  int N = 4; //Matrix size
+int matrixNorm(){
+  int N = 6000; //Matrix size
   
   srand(300); // Fixed Seed
+
   // allocate memory in host RAM
   int *A, *B;
   A = (int*)malloc(N*N*sizeof(int));
-  B = (int*)malloc(N*N*sizeof(int));
-  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-  dim3 dimGrid(GRID_SIZE, GRID_SIZE); 
+  B = (int*)malloc(N*N*sizeof(int)); 
   //generate matrix A
-  printf("generating A\n");
   for(int i = 0; i < N; ++i){
     for(int j = 0; j < N; ++j){
       A[i * N + j] = rand() / 32768;
     }
   }
-
-  printf("A generated\n");
   printMatrix(A, N);
-  printMatrix(B, N); 
-  float gpu_elapsed_time_ms;
-
-  //events to count the execution time
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+  printMatrix(B, N);
   
   //allocate memory on the device
   int *d_a, *d_b;
-  cudaMalloc((void **)&d_a, sizeof(int)*N*N);
-  cudaMalloc((void **)&d_b, sizeof(int)*N*N);
-
-
-  cudaMemcpy(d_a, A, sizeof(int)*N*N, cudaMemcpyHostToDevice);
-  
-  cudaEventRecord(start, 0);
+  cudaMalloc(&d_a, sizeof(int)*N*N);
+  cudaMalloc(&d_b, sizeof(int)*N*N);
+  int i, j;
+  for(i=0; i<N; i++){
+    for(j=0; j<N; j++){
+      cudaMemcpy(&d_a[i*N+j], (void*)&A[i*N+j], sizeof(int), cudaMemcpyHostToDevice);
+    }
+  }
   // Call Matrix Norm function
-  int numBlocks = N / 2;
-  int numThreads = N / 2;
-  gpu_matrixNorm<<<dimBlock, dimGrid>>>(d_a, d_b, N);
-
-  cudaEventRecord(stop, 0);
-  cudaEventSynchronize(stop);
+  int numThreads = 128;
+  int numBlocks = ((N*N) + numThreads-1) / numThreads;
+  gpu_matrixNorm<<<numBlocks, numThreads>>>(d_a, d_b, N);
   
-  cudaMemcpy(B, d_b, sizeof(int)*N*N, cudaMemcpyDeviceToHost);
-
-  cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
+  for(i=0; i<N; i++){
+    for(j=0; j<N; j++){
+      cudaMemcpy((void *)&B[i*N+j], &d_b[i*N+j], sizeof(int), cudaMemcpyDeviceToHost);
+    }
+  }
   printMatrix(B, N);
-  printf("Runtime = %f ms.\n", gpu_elapsed_time_ms);
   
   cudaFree(d_a);
   cudaFree(d_b);
   free(A);
   free(B);
   return 0;
+}
+
+int main(int argc, char const *argv[]){
+  struct timeval etstart, etstop;  /* Elapsed times using gettimeofday() */
+  struct timezone tzdummy;
+  unsigned long long time;
+  gettimeofday(&etstart, &tzdummy);
+
+  matrixNorm();
+
+  gettimeofday(&etstop, &tzdummy);
+  time = (unsigned long long)(etstop.tv_sec - etstart.tv_sec)*1000000+(etstop.tv_usec - etstart.tv_usec);
+  printf("Runtime = %g ms.\n", (float)time/(float)1000);
 }
